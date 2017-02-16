6d2e97bfb995891a8e9fb28fa3aa3898c2b83e47
nicolecheng <ncheng@stuy.edu>
2017-02-16 09:49:33 -0500

    a

--- 021617/canvas.js
+++ 021617/canvas.js
@@ -1,13 +1,42 @@
 var button = document.getElementById("b"); 
 var c = document.getElementById("canvas");
 
-var circle = function(){
+
+var clear = function(){
     var ctx = c.getContext('2d');
     ctx.beginPath();
-    ctx.fillStyle = "#FF0000";
-    ctx.arc(100,100,20,0,2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
+    ctx.clearRect(0,0,c.clientWidth,c.clientHeight);	
+}
+
+button.addEventListener("click", stop);
+
+var rid;
+
+var animateDot = function(){
+    var x = 0;
+
+    var stop = function(){
+	window.cancelAnimationFrame(rid);
+    }
+
+    var circle = function(){
+	clear();
+	var ctx = c.getContext('2d');
+	ctx.beginPath();
+	ctx.fillStyle = "#FF0000";
+	ctx.arc(x,c.height/2,20,0,2*Math.PI);
+	ctx.fill();
+	ctx.stroke();
+	x++;
+	rid = window.requestAnimationFrame( circle );
+    };
+
+    circle();
+
 };
 
-circle();
+//circle();
+
+//window.requestAnimationFrame( circle );
+
+c.addEventListener('click',animateDot);
